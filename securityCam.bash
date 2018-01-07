dc3b4d59b5773d4169804b9f4398f781bf94d20e
isa sand <isasandh@gmail.com>
2018-01-07 10:23:23 +0100

    sending to cloud

--- securityCam.bash
+++ securityCam.bash
@@ -1,20 +1,18 @@
 raspistill -v -o newpic.jpg
+cp newpic.jpg differImage.jpg
 convert newpic.jpg newpic.rgba
 answer=`cmp oldpic.rgba newpic.rgba 2>&1`
 echo $answer
 if [[ "$answer" == "cmp: oldpic.rgba: No such file or directory" ]]; then
         mv newpic.rgba oldpic.rgba
+        rm differImage.jpg
+        rm newpic.jpg
 fi
 if [[ "$answer" == *"differ"* ]]; then
         mv newpic.rgba oldpic.rgba
-        echo "send to clooud mf"
+        echo `date` > date.txt
+        rm newpic.jpg
+        convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% differImage.jpg differImage.jpg
+        python sendToS3.py
 fi
-
-
-
-
-
-
-
-
 
